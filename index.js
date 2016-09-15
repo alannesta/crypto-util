9fe973ac09d4d3179b53dca9f9ef6f5beac5e6d7
Alan Cao <alannesta@sina.com>
2016-09-15 16:02:06 -0400

    elastic dump util

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var program = require('commander');
 var AESdecrypt = require('./crypto-util/aes-util').decrypt;
 var AESencrypt = require('./crypto-util/aes-util').encrypt;
 
+var ElasticUtil = require('./elastic-util/elastic-util');
+
 program
 	.command('encrypt')
 	.description('encrypt files')
@@ -27,12 +29,25 @@ program
 	.option("-d, --destination  <outputfile>", "path to output the decrypted file")
 	.option("-p, --password <password>", "password used")
 	.action(function(options){
-		console.log(options.source);
-		console.log(options.destination);
-		console.log(options.password);
 		if (options.source && options.destination && options.password) {
 			try {
 				AESdecrypt(options.source, options.destination, options.password);
+			} catch(err) {
+				console.log(err);
+			}
+		}
+	});
+
+program
+	.command('elastic-dump')
+	.description('dump/import elasticsearch data')
+	.option("-s, --source <sourcefile>", "source url of the index to be dumped")
+	.option("-d, --destination  <outputfile>", "path to output the dumped data (json format)")
+	.option("-t, --type  <type>", "the type of data to be dumped, one of 'data', 'mapping', 'analyzer'")
+	.action(function(options){
+		if (options.source && options.destination && options.type) {
+			try {
+				ElasticUtil.dump(options.source, options.destination, options.type);
 			} catch(err) {
 				console.log(err);
 			}
